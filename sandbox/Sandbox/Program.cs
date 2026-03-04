1e7e5fd8809652c8c6458124fad0bf4e966a27fe
KaylieT2025 <ktapia@byupathway.edu>
2026-03-04 09:51:20 -0500

    Completed Exersise #1 for week 1 and playing in Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}! I'm Casey Sharp, I'm your C# buddy!");
+
     }
 }
