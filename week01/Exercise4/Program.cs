ea1fc20716c7fb31d6ff6cd203d16058ab015fb9
KaylieT2025 <ktapia@byupathway.edu>
2026-03-04 17:13:10 -0500

    Core requirments for week 1 exersise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        //List to add users numbers to a list.
+
+        List<int> userNumbers = new List<int>();
+        Console.WriteLine("Please enter a number. Enter 0 to finish.");
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == 0)
+            {
+                Console.WriteLine("Data Entery Ended.");
+                int sum = userNumbers.Sum();
+                Console.WriteLine($"The sum is: {sum}");
+                double average = userNumbers.Average();
+                Console.WriteLine($"The average is: {average}");
+                int maxium = userNumbers.Max();
+                Console.WriteLine($"The biggest number enter is: {maxium}");
+                break;
+            }
+            userNumbers.Add(userNumber);
+            
+        }
     }
 }
