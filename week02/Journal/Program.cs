2a23de1187e008b55fd02fdff4c9d04fb9ddd2b5
KaylieT2025 <ktapia@byupathway.edu>
2026-03-11 14:17:34 -0400

    Week 02 Learning Activity: Abstraction

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        //#1-Random prompt from list. Save response and date as Entry: Date.
+        //#2-Display all journal entries.
+        //#3-Prompt for a file name and save to list.
+        //#4-Prompt for a file name to disply all entries from that file.
+        //#5-Menu to choose from actions.
+        //#6-At least 5 diffrent prompts in the list.
+        //Make sure interface looks like the video.
+
+        /*Create a class for every major componet. Contain at least 2 classes in addition to Program class.
+        Use member variables and methods appropiatly.*/
+
+        //Remember to show creativity to get 100%.
     }
 }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Buffers;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._companyName = "Microsoft";
+        job1._jobTitle = "Softwear Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._companyName = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2026;
+        job2._endYear = 2036;
+
+        //Displays company name.
+        //Console.WriteLine(job1._companyName);
+        //Console.WriteLine(job2._companyName);
+
+        //Displays job title (Company) start date-end date.
+        //job1.Display();
+        //job2.Display();
+
+        Resume resume = new Resume();
+
+        resume._name = "Penny";
+
+        resume._jobList.Add(job1);
+        resume._jobList.Add(job2);
+
+        //Displays just the job title.
+        //Console.WriteLine(resume._jobList[0]._jobTitle);
+        //Console.WriteLine(resume._jobList[1]._jobTitle);
+
+        //Final display all information.
+        resume.Display();
     }
 }
