9520d6a4c8bba8e8d9bbf906d16257c07c83e430
KaylieT2025 <ktapia@byupathway.edu>
2026-03-19 16:59:00 -0400

    Stubs

--- week03/ScriptureMemorizer/Refrence.cs
+++ week03/ScriptureMemorizer/Refrence.cs
@@ -5,5 +5,25 @@ public class Refrence
     private string _book;
     private int _chapter;
     private int _verse;
-    private int _endVerse; 
+    private int _endVerse;
+
+    public Refrence(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    } 
+
+    public Refrence (string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
 }
--- week03/ScriptureMemorizer/Scriptures.cs
+++ week03/ScriptureMemorizer/Scriptures.cs
@@ -1,7 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 public class Scriptures
 {
-    public string _Scriptures;
+    private Refrence _refrence;
+    private List<Word> _words;
+    public Scriptures(Refrence refrence, string text)
+    {
+
+    }
+    public void HideRandomWords()
+    {
+
+    }
+    public string GetDisplayText()
+    {
+        return "";
+    }
+    public bool IsCompletlyHidden()
+    {
+        return false;
+    }
 
 }
