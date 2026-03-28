04dc7c69e152f5249d48570e6795a95a734fcc94
KaylieT2025 <ktapia@byupathway.edu>
2026-03-28 16:42:34 -0400

    Functioning YouTube code.

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -5,7 +5,7 @@ public class Comment{
     private string _name;
     private string _commentText;
 
-    public Comment (string name, string commentText){
+    public Comment(string name, string commentText){
 
         _name = name;
         _commentText = commentText;
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,31 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
-        Video video = new Video();
+        //Video video = new Video();
 
         //Videos
-        Video video1 = new video ("Programming", "Penny", 300);
-        Video video2 = new video ("Animation", "Selvester Stone", 900);
-        Video video3 = new video ("Dress Making", "Kimmy", 750);
+        Video video1 = new Video("Programming", "Penny", 300);
+        Video video2 = new Video("Animation", "Selvester Stone", 900);
+        Video video3 = new Video("Dress Making", "Kimmy", 750);
 
         //Comments for video1
-        video1.AddComment(new Comment("Mary Anne", "Good information"));
-        video1.AddComment(new Comment("JJ327", "Very helpful!"));
-        video1.AddComment(new Comment("Daniel", "Can you do a video on encapulation next?"));
+        video1.AddComments(new Comment("Mary Anne", "Good information"));
+        video1.AddComments(new Comment("JJ327", "Very helpful!"));
+        video1.AddComments(new Comment("Daniel", "Can you do a video on encapulation next?"));
 
         //Comments for video2
-        video2.AddComment(new Comment("Jack", "This is very informative"));
-        video2.AddComment(new Comment("Lucy", "What programs do you use?"));
-        video2.AddComment(new Comment("Mira4567zzy", "Nice modeling."));
+        video2.AddComments(new Comment("Jack", "This is very informative"));
+        video2.AddComments(new Comment("Lucy", "What programs do you use?"));
+        video2.AddComments(new Comment("Mira4567zzy", "Nice modeling."));
 
         //Comments for video3
-        video3.AddComment(new Comment("Hailey", "I love it!"));
-        video3.AddComment(new Comment("Matt", "Where do I get that fabric?"));
-        video3.AddComment (new Comment("Ben", "How do I get them template?"));
+        video3.AddComments(new Comment("Hailey", "I love it!"));
+        video3.AddComments(new Comment("Matt", "Where do I get that fabric?"));
+        video3.AddComments(new Comment("Ben", "How do I get them template?"));
 
         //Video list
         List<Video> videos = new List<Video>{
