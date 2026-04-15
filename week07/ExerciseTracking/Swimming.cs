cf02039e72974b02388f722f2375ed7633e9dc7b
KaylieT2025 <ktapia@byupathway.edu>
2026-04-15 15:06:12 -0400

    Swimming class setup.

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (_laps * 50) / 1000 * 0.06;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate()} Swimming ({GetMinutes()} min) Distance: {GetDistance():F1} Speed: {GetSpeed():F1} Pace: {GetPace():F1}";
+    }
+}
